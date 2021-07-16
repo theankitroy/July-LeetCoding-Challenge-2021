637f94ff56b940be024ea42c76e04bdd41026f0f
Ankit Kumar <ankitroy035@gmail.com>
2021-07-16 23:04:15 +0530

    Added "Valid Triangle Number" Solution

---
+++ ValidTriangleNumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length, ans = 0;
+        for (int k = 2; k < n; ++k) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    ans += j - i;
+                    j -= 1;
+                } else {
+                    i += 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
